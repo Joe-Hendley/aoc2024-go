394557a6df0c453d0ac5d99ff2ffed8df6bee7cf
Joe Hendley <95080839+Joe-Hendley@users.noreply.github.com>
2024-12-02 11:18:32 +0000

    part1

---
+++ internal/aoc/solvers/part1/solve.go
@@ -0,0 +1,77 @@
+package part1
+
+import (
+	"slices"
+	"strings"
+
+	"github.com/Joe-Hendley/aoc2024/internal/aoc/dangerconv"
+	"github.com/Joe-Hendley/aoc2024/internal/aoc/ints"
+	"github.com/Joe-Hendley/aoc2024/internal/aoc/logger"
+)
+
+type Solver struct {
+	logger.Logger
+}
+
+func (s *Solver) Init(verbose bool) {
+	s.Logger = logger.New(verbose)
+
+	return
+}
+
+func arrangeInput(input string) ([]int, []int) {
+	lines := strings.Split(input, "\n")
+
+	lhs := []int{}
+	rhs := []int{}
+
+	for _, line := range lines {
+		parts := strings.Fields(line)
+		lhnum := dangerconv.Atoi(parts[0])
+		rhnum := dangerconv.Atoi(parts[1])
+
+		lhs = append(lhs, lhnum)
+		rhs = append(rhs, rhnum)
+	}
+
+	return lhs, rhs
+}
+
+func (s *Solver) Part1(input string) int {
+	lhs, rhs := arrangeInput(input)
+
+	slices.Sort(lhs)
+	slices.Sort(rhs)
+
+	total := 0
+	for i := range lhs {
+		total += ints.Distance(lhs[i], rhs[i])
+	}
+
+	return total
+}
+
+func (s *Solver) Part2(input string) int {
+	lhs, rhs := arrangeInput(input)
+
+	rhsCounts := make(map[int]int)
+
+	for i := range rhs {
+		_, ok := rhsCounts[rhs[i]]
+		if ok {
+			rhsCounts[rhs[i]] += 1
+		} else {
+			rhsCounts[rhs[i]] = 1
+		}
+	}
+
+	total := 0
+	for i := range lhs {
+		count, ok := rhsCounts[lhs[i]]
+		if ok {
+			total += (lhs[i] * count)
+		}
+	}
+
+	return total
+}
---
+++ internal/aoc/solvers/part1/solve_test.go
@@ -0,0 +1,30 @@
+package part1
+
+import (
+	"testing"
+
+	"github.com/Joe-Hendley/aoc2024/internal/aoc/assert"
+	"github.com/Joe-Hendley/aoc2024/internal/aoc/file"
+)
+
+func TestPartOne(t *testing.T) {
+	input := file.DangerReadToString("test.txt")
+	want := 11
+	solver := Solver{}
+	solver.Init(true)
+
+	got := solver.Part1(input)
+
+	assert.Equal(t, got, want)
+}
+
+func TestPartTwo(t *testing.T) {
+	input := file.DangerReadToString("test.txt")
+	want := 31
+	solver := Solver{}
+	solver.Init(true)
+
+	got := solver.Part2(input)
+
+	assert.Equal(t, got, want)
+}
